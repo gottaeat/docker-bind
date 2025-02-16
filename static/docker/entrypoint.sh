9871442a72996002e583080795a51a7691cd5952
must_eat <eateing@loggedin.net>
2025-02-16 20:53:39 +0300

    upd: (static/) docker/entrypoint.sh; set ownership to named:named for /data.

--- static/docker/entrypoint.sh
+++ static/docker/entrypoint.sh
@@ -9,10 +9,16 @@ if mountpint /data >/dev/null 2>&1; then
 fi
 
 # - - hand over - - #
-pinfo "setting permissions"
+pinfo "setting permissions for dirs under /data"
 find /data -type d -exec chmod 0750 {} ';'
 evalret
+
+pinfo "setting permissions for files under /data"
 find /data -type f -exec chmod 0640 {} ';'
+evalret
+
+pinfo "setting ownership of /data"
+chown -Rh named:named /data
 evalret
 
 pinfo "starting named"
